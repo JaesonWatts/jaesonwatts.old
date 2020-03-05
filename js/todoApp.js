27154db607ee83febd7205d86dbf913e426edcea
jaesonwatts <illender.the.great@gmail.com>
2020-03-05 10:02:16 -0700

    todoApp to V4 again

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,44 +1,34 @@
 var todoList = {
-    todos: ['item 1', 'item 2', 'item 3'],
-    displayTodos: function() {
-      console.log('My todos: ', this.todos);
-    },
-    addTodo: function(todo) {
-      this.todos.push(todo);
-      this.displayTodos();
-    },
-    changeTodo: function(position, newValue) {
-      this.todos[position] = newValue;
-      this.displayTodos();
-    },
-    deleteTodo: function(position) {
-      this.todos.splice(position, 1);
-      this.displayTodos();
-    }
-  };
+  todos: [],
+  displayTodos: function() {
+    console.log('My todos: ', this.todos);
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+  }
+};
 
 
-// var todos = []
+// V4 req: todoList.addTodo should add objects *
+// toList.changeTodo should change the todoText property *
+// todoList.toggleCompleted should change the completed property *
 
-// // it should have a function to display the todos.
-// function displayTodos() {
-//     console.log(todos);
-// }
-
-// // it should have a function to add todos.
-// function addTodo(todo) {
-//     todos.push(todo);
-//     displayTodos();
-// }
-
-// // it should have a function to change todos.
-// function changeTodo(position, newValue) {
-//     todos[position] = newValue;
-//     displayTodos();
-// }
-
-// // it should have a function to delete todos.
-// function deleteTodo(position) {
-//     todos.splice(position, 1);
-//     displayTodos();
-// }
+// V5 Req: .displayTodos should show .todoText
+// .displayTodos should tess you if .todos is empty
+// .displayTodos should show .completed
