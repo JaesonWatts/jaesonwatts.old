1fcbc22e91d37aaf63cee1d189db9b530f614d60
jaesonwatts <illender.the.great@gmail.com>
2020-03-05 10:12:11 -0700

    todoApp V5 done(again)

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,7 +1,18 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My todos: ', this.todos);
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!')
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if(this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )',this.todos[i].todoText);
+        } 
+      }
+    } 
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -29,6 +40,6 @@ var todoList = {
 // toList.changeTodo should change the todoText property *
 // todoList.toggleCompleted should change the completed property *
 
-// V5 Req: .displayTodos should show .todoText
-// .displayTodos should tess you if .todos is empty
-// .displayTodos should show .completed
+// V5 Req: .displayTodos should show .todoText*
+// .displayTodos should tess you if .todos is empty*
+// .displayTodos should show .completed*
